2bd4d908225d52bced47f4e202406c3fc289690b
Caleb Overmyer <C.p.overmyer@outlook.com>
2026-08-03 02:28:22 -0700

    refactored user-role-code.controller to comply with asyncHandler, despite deprication

--- API/src/features/user-role-codes/user-role-code.controller.ts
+++ API/src/features/user-role-codes/user-role-code.controller.ts
@@ -8,36 +8,29 @@
  */
 import { Request, RequestHandler, Response } from 'express';
 import * as userRoleCodeService from './user-role-code.service';
+import { asyncHandler } from '../../middleware/async-handler.middleware';
 
 /**
  * Controller Method to format response body from Service data
  * @param req Request Body
  * @param res Response Body
  */
-export const getUserRoleCodes: RequestHandler = async (req: Request, res: Response) => {
-    try {
-        const userRoles = await userRoleCodeService.getUserRoles();
-        console.log('[userRoleCodeController][getUserRolesCodes][Success]: ', userRoles);
-        res.status(200).json(userRoles);
-    } catch (error) {
-        console.log('[userRoleCodeController][getUserRolesCodes][Error]: ', error);
-        res.status(500).json({ error: 'Failed to fetch user roles' });
-    }
-};
+export const getUserRoleCodes: RequestHandler = asyncHandler(async (req, res) => {
+    const userRoles = await userRoleCodeService.getUserRoles();
+    return res.json(userRoles);
+});
 
 /**
  * Controller Method to format response body from Service data
  * @param req Request Body
  * @param res Response Body
  */
-export const getUserRoleCodeById: RequestHandler = async (req: Request, res: Response) => {
+export const getUserRoleCodeById: RequestHandler = asyncHandler(async (req, res) => {
     const role_id = parseInt(req.params.role_id as string, 10);
-    try {
-        const userRole = await userRoleCodeService.getUserRole(role_id);
-        console.log('[userRoleCodeController][getUserRoleCodeById][Success]: ', userRole[0]);  
-        res.status(200).json(userRole[0]); // Assuming the service returns an array, we send the first element
-    } catch (error) {
-        console.log('[userRoleCodeController][getUserRoleCodeById][Error]: ', error);
-        res.status(500).json({ error: 'Failed to fetch user role' });
+    if (!role_id) {
+        return res.status(400).json({ error: 'Invalid role_id parameter' });
     }
-};
+
+    const userRoles = await userRoleCodeService.getUserRole(role_id);
+    return res.json(userRoles[0]); // Assuming the service returns an array, we send the first element
+});
